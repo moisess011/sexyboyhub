fd8d9f63484caaebdf28282bd774133e6be7fcce
moisess011 <tutorial8000@gmail.com>
2026-07-28 19:50:58 -0300

    Key.lua

--- Key.lua
+++ Key.lua
@@ -23,7 +23,7 @@ local Config = {
     Secret          = "49032246", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
-    MainScriptURL   = "Link", -- The raw URL of your main script
+    MainScriptURL   = "https://raw.githubusercontent.com/moisess011/sexyboyhub/refs/heads/main/script.lua", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
@@ -39,12 +39,12 @@ local Config = {
     KeyFileName     = "Mykey.txt", -- The name of the file where the valid key will be saved for auto-login
 
     -- [6] GUI Management
-    OldGuiName      = "", -- Name of the old GUI to destroy if it's already open
-    MainGuiName     = "", -- Name of the main script's GUI to check if it's already executing
+    OldGuiName      = "setsoboy", -- Name of the old GUI to destroy if it's already open
+    MainGuiName     = "setsoboy", -- Name of the main script's GUI to check if it's already executing
 
     -- [7] Hub Information & UI Text
-    HubName         = "Test", -- The main title shown at the top of the GUI
-    HubDescription  = "Test" -- The text shown below the title
+    HubName         = "SexyBoy HUB", -- The main title shown at the top of the GUI
+    HubDescription  = "Q ondax!" -- The text shown below the title
 }
 
 -------------------------------------------------------------------------------
